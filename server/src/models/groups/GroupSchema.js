cbd31ac1ee3813c0b058c0d9f74f682539059daf
Yves Robert Sta Ana <ystaana@agsx.net>
2019-04-23 00:26:54 +0800

    more validations

--- server/src/models/groups/GroupSchema.js
+++ server/src/models/groups/GroupSchema.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var GroupSchema = new mongoose.Schema({
-  name: { type : String, required : true},
+  name: { type : String, required : true, unique : true},
   description: String,
   students : [
     {
